f8a1e479c57448ce968e38dd9022091f91849b38
Brandon Alvarez <brandonalvarex15@gmail.com>
2025-10-29 20:22:54 -0500

    Week 6 Done

--- Week6Assignment/script.js
+++ Week6Assignment/script.js
@@ -1,3 +1,5 @@
+//Brandon Alvarez OCT 29 2025
+
 const input = document.getElementById("itemInput")
 const addBtn = document.getElementById("addBtn")
 const list = document.getElementById("list")
@@ -11,3 +13,43 @@ function updateCounter(){
     const count = items.length;
     counter.textContent = `${count} item${count !== 1 ? 's' : ''}`;
 }
+
+addBtn.addEventListener("click", () =>{
+    const text = input.value.trim();
+    if (text === "") return;
+
+    const li = document.createElement("li");
+    li.textContent = text;
+
+    li.addEventListener("click", () => {
+        li.classList.toggle("done");
+    });
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "✖";
+    deleteBtn.className = "delete-btn";
+
+    deleteBtn.addEventListener("click", (e) => {
+        e.stopPropagation();
+        li.remove();
+        updateCounter();
+    });
+
+    li.appendChild(deleteBtn);
+    list.appendChild(li);
+    input.value = "";
+    updateCounter();
+});
+
+input.addEventListener("keypress", (e) => {
+    if (e.key === "Enter") {
+        addBtn.click();
+    }
+});
+
+clearBtn.addEventListener("click", () => {
+    list.innerHTML = "<strong></strong>";
+    updateCounter();
+});
+
+updateCounter();
